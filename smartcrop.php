fa42eed55db8f23f13cea2f49d0b30f981cf6f45
Alex Mills <Viper007Bond@users.noreply.github.com>
2018-01-31 19:09:10 -0800

    Initial commit

---
+++ smartcrop.php
@@ -0,0 +1,30 @@
+<?php /*
+
+**************************************************************************
+
+Plugin Name:  SmartCrop
+Description:
+Plugin URI:   https://alex.blog/wordpress-plugins/smartcrop/
+Version:      1.0.0
+Author:       Alex Mills (Viper007Bond)
+Author URI:   https://alex.blog/
+Text Domain:  smartcrop
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+
+**************************************************************************
+
+SmartCrop is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+SmartCrop is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SmartCrop. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+
+**************************************************************************/
